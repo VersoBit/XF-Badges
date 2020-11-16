086b5b6b3d1ea04257e0d7d6b3f89dac2042e1ab
Rylea Stark <rylea@versobit.com>
2020-11-16 01:14:55 -0700

    Start of Activity Intervals + Formatting
    #10 Starts adding functionality for intervals.

--- Cron/Badge.php
+++ Cron/Badge.php
@@ -10,56 +10,54 @@ use CMTV\Badges\Constants as C;
 use CMTV\Badges\Repository\UserBadge;
 use CMTV\Badges\XF\Entity\User;
 
-class Badge
-{
-    public static function runBadgeCheck()
-    {
+class Badge {
+    public static function runBadgeCheck() {
         /** @var \CMTV\Badges\Repository\Badge $badgeRepo */
         $badgeRepo = \XF::repository(C::__('Badge'));
 
         $badges = $badgeRepo->findBadgesForList()->fetch();
 
-        if (!$badges)
-        {
+        if (!$badges) {
             return;
         }
 
         $userFinder = \XF::finder('XF:User');
-        //TODO: Add settings to adjust activity filter (or turn it off)
-        $users = $userFinder
-            ->where('last_activity', '>=', time() - 2 * 3600)
-            ->isValidUser(false)
-            ->fetch();
+        $cronInterval = \XF::option('vBbadgesCronActivityInterval');
+
+        if ($cronInterval == 0) {
+            //TODO: No Activity Query
+            $users = $userFinder->fetch();
+        } else {
+            $users = $userFinder
+                ->where('last_activity', '>=', time() - $cronInterval * 3600)
+                ->isValidUser(false)
+                ->fetch();
+        }
 
         /** @var User $user */
-        foreach ($users as $user)
-        {
+        foreach ($users as $user) {
             /** @var UserBadge $userBadgeRepo */
             $userBadgeRepo = \XF::repository(C::__('UserBadge'));
 
             $awardedIds = $userBadgeRepo->getAwardedBadgeIds($user->user_id);
 
             /** @var \CMTV\Badges\Entity\Badge $badge */
-            foreach ($badges as $badge)
-            {
-                if (in_array($badge->badge_id, $awardedIds))
-                {
+            foreach ($badges as $badge) {
+                if (in_array($badge->badge_id, $awardedIds)) {
                     continue;
                 }
 
                 $userCriteria = \XF::app()->criteria('XF:User', $badge->user_criteria);
                 $userCriteria->setMatchOnEmpty(false);
 
-                if ($userCriteria->isMatched($user))
-                {
+                if ($userCriteria->isMatched($user)) {
                     $userBadgeRepo->awardWithBadge($user, $badge->badge_id);
                 }
             }
         }
     }
 
-    protected function getUserBadgeRepo(): UserBadge
-    {
+    protected function getUserBadgeRepo(): UserBadge {
         return \XF::repository(C::__('UserBadge'));
     }
 }
